f06b10c42aafb9b7c22b262725281eec8889efe8
harutiro <harutiro2027@outlook.jp>
2022-07-18 18:24:12 +0900

    ラジオボタン形式の問題完全完了

--- linguaporta.py
+++ linguaporta.py
@@ -11,8 +11,7 @@ id = input()
 print("パスワードを入力してください。")
 pw = input()
 
-print("問題を選択したらEnterを押してください。")
-input()
+print("問題を選択したらEnterを押してください。終わる時はendを入力してください。")
 
 
 browser = webdriver.Chrome()
@@ -37,102 +36,132 @@ browser.execute_script('arguments[0].click();', elem_login_btn)
 elem_login_btn = browser.find_element(By.CLASS_NAME,'btn.btn-reference-select')
 browser.execute_script('arguments[0].click();', elem_login_btn)
 
-elem_login_btn = browser.find_element(By.XPATH,'//*[@id="content-study"]/div[1]/div[10]/div[1]/div/input')
-browser.execute_script('arguments[0].click();', elem_login_btn)
-
-# DB
-dbname = 'linguaporta.db'
-conn = sqlite3.connect(dbname)
-cur = conn.cursor()
-
-tableName = 'q31'
-try:
-    cur.execute(f'CREATE TABLE {tableName}(question INTEGER PRIMARY KEY , ans STRING)')
-except:
-    print("もう既にDBは作られています。")
-
-values = []
-questions = []
-
-# ここから答えを取得
 while True:
-    sleep(1)
-
-    elem_question = browser.find_element(By.XPATH,'//*[@id="question_td"]/form[1]/b')
-    question_ = int(elem_question.text.split('：')[1])
-    cur.execute(f'select * from {tableName} where question == {question_};')
-    print(cur.fetchall())   
-    
-    cur.execute(f'select * from {tableName} where question == {question_};')
-    if len(cur.fetchall())==0:
-        elem_radio_0 = browser.find_element(By.ID,'answer_0_0')
-        elem_radio_0.click()
-
-        elem_push = browser.find_element(By.ID,'ans_submit')
-        elem_push.click()
-
-        sleep(1)
+    # 問題選択待ち
+    inputStr = input()
+    if inputStr == "end":
         try:
-            sleep(1)
-            elem_ans_btn = browser.find_element(By.XPATH,'//input[@value=\'正解を見る\']')
-            browser.execute_script('arguments[0].click();', elem_ans_btn)
+            conn.close()
         except:
-            print("no answer")
+            print("DB接続エラー")
+        break
 
-        elem_ans = browser.find_element(By.ID,'drill_form')
-        value = elem_ans.text.split('：')[1].split('\n')[0].replace('○','')
-        values.append(value)
+    # 問題確認
+    elem_title = browser.find_element(By.CLASS_NAME,'bloc-resp.bloc-resp-lessonname')
+    print(elem_title.text)
+
+    title_number = elem_title.text.split(' ')[0]
+    title_content = elem_title.text.split(' ')[1]
+    tableName = ""
+
+    if title_content == '多肢選択':
+        tableName = f'Q{title_number}1'
+    elif title_content == '単一選択':
+        tableName = f'Q{title_number}2'
+    elif title_content == '記述問題':
+        tableName = f'Q{title_number}3'
+    elif title_content == '記述問題':
+        tableName = f'Q{title_number}4'
+
+    # DB
+    dbname = 'linguaporta.db'
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+
+    try:
+        cur.execute(f'CREATE TABLE {tableName}(question INTEGER PRIMARY KEY , ans STRING)')
+    except:
+        print("もう既にDBは作られています。")
+
+    values = []
+    questions = []
+
+    # ここから答えを取得
+    while True:
+        sleep(1)
 
         elem_question = browser.find_element(By.XPATH,'//*[@id="question_td"]/form[1]/b')
-        question = int(elem_question.text.split('：')[1])
-        questions.append(question)
-
-        # print(type(question))
-        # print(type(value))
-
-        cur.execute(f'INSERT INTO {tableName}(question , ans) values( {question} , \'{value}\' )')
-
-        # print(values)
-        # print(questions)
-        # 中身を全て取得するfetchall()を使って、printする。
-        cur.execute(f'SELECT * FROM {tableName}')
-        print(cur.fetchall())   
-    else:
-        elem_radio_0 = browser.find_element(By.ID,'answer_0_0')
-        elem_label_0 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[1]')
-        elem_radio_1 = browser.find_element(By.ID,'answer_0_1')
-        elem_label_1 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[2]')
-        elem_radio_2 = browser.find_element(By.ID,'answer_0_2')
-        elem_label_2 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[3]')
-        elem_radio_3 = browser.find_element(By.ID,'answer_0_3')
-        elem_label_3 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[4]')
-
+        question_ = int(elem_question.text.split('：')[1])
         cur.execute(f'select * from {tableName} where question == {question_};')
-        ansCheck = cur.fetchall()[0][1]
-
-        if elem_label_0.text == ansCheck:
-            browser.execute_script('arguments[0].click();', elem_radio_0)
-
-        elif elem_label_1.text == ansCheck:
-            browser.execute_script('arguments[0].click();', elem_radio_1)
-
-        elif elem_label_2.text == ansCheck:
-            browser.execute_script('arguments[0].click();', elem_radio_2)
-
-        elif elem_label_3.text == ansCheck:
-            browser.execute_script('arguments[0].click();', elem_radio_3)
-
-        elem_push = browser.find_element(By.ID,'ans_submit')
-        elem_push.click()
-
-    
-    sleep(1)
-    elem_next_btn = browser.find_element(By.XPATH,'//input[@value=\'次の問題へ\']')
-    browser.execute_script('arguments[0].click();', elem_next_btn)
-    conn.commit()
+        print(cur.fetchall())   
         
+        cur.execute(f'select * from {tableName} where question == {question_};')
+        if len(cur.fetchall())==0:
+            elem_radio_0 = browser.find_element(By.ID,'answer_0_0')
+            elem_radio_0.click()
 
-conn.commit()
-conn.close()
+            elem_push = browser.find_element(By.ID,'ans_submit')
+            elem_push.click()
+
+            sleep(1)
+            try:
+                sleep(1)
+                elem_ans_btn = browser.find_element(By.XPATH,'//input[@value=\'正解を見る\']')
+                browser.execute_script('arguments[0].click();', elem_ans_btn)
+            except:
+                print("no answer")
+
+            elem_ans = browser.find_element(By.ID,'drill_form')
+            value = elem_ans.text.split('：')[1].split('\n')[0].replace('○','')
+            values.append(value)
+
+            elem_question = browser.find_element(By.XPATH,'//*[@id="question_td"]/form[1]/b')
+            question = int(elem_question.text.split('：')[1])
+            questions.append(question)
+
+            # print(type(question))
+            # print(type(value))
+
+            cur.execute(f'INSERT INTO {tableName}(question , ans) values( {question} , \'{value}\' )')
+
+            # print(values)
+            # print(questions)
+            # 中身を全て取得するfetchall()を使って、printする。
+            cur.execute(f'SELECT * FROM {tableName}')
+            print(cur.fetchall())   
+        else:
+            elem_radio_0 = browser.find_element(By.ID,'answer_0_0')
+            elem_label_0 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[1]')
+            elem_radio_1 = browser.find_element(By.ID,'answer_0_1')
+            elem_label_1 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[2]')
+            elem_radio_2 = browser.find_element(By.ID,'answer_0_2')
+            elem_label_2 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[3]')
+            elem_radio_3 = browser.find_element(By.ID,'answer_0_3')
+            elem_label_3 = browser.find_element(By.XPATH,'//*[@id="drill_form"]/label[4]')
+
+            cur.execute(f'select * from {tableName} where question == {question_};')
+            ansCheck = cur.fetchall()[0][1]
+
+            if elem_label_0.text == ansCheck:
+                browser.execute_script('arguments[0].click();', elem_radio_0)
+
+            elif elem_label_1.text == ansCheck:
+                browser.execute_script('arguments[0].click();', elem_radio_1)
+
+            elif elem_label_2.text == ansCheck:
+                browser.execute_script('arguments[0].click();', elem_radio_2)
+
+            elif elem_label_3.text == ansCheck:
+                browser.execute_script('arguments[0].click();', elem_radio_3)
+
+            elem_push = browser.find_element(By.ID,'ans_submit')
+            elem_push.click()
+
+        
+        try:
+            sleep(1)
+            elem_next_btn = browser.find_element(By.XPATH,'//input[@value=\'次の問題へ\']')
+            browser.execute_script('arguments[0].click();', elem_next_btn)
+            conn.commit()
+        except:
+            print("end")
+            print("==========================================================")
+            print("問題を選択したらEnterを押してください。終わる時はendを入力してください。")
+
+            break
+
+    conn.commit()
+
+
 # elem_login_btn = browser.find_element(By.XPATH,'//*[@id="content-study"]/div[1]/div[3]/div[1]/div/input')
 # elem_login_btn.click()
